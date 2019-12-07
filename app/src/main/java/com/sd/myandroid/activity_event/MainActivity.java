50767c247e9c3096ba54d3d778ecec79fc186470
zjun <565061763@qq.com>
2019-12-07 17:08:01 +0800

    demo

--- app/src/main/java/com/sd/myandroid/activity_event/MainActivity.java
+++ app/src/main/java/com/sd/myandroid/activity_event/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.sd.lib.eventact.observer.ActivityCreatedObserver;
 import com.sd.lib.eventact.observer.ActivityDestroyedObserver;
 
 public class MainActivity extends BaseActivity
@@ -13,11 +14,21 @@ public class MainActivity extends BaseActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        mActivityCreatedObserver.register();
         mActivityDestroyedObserver.register();
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private final ActivityCreatedObserver mActivityCreatedObserver = new ActivityCreatedObserver(this)
+    {
+        @Override
+        public void onActivityCreated(Activity activity, Bundle savedInstanceState)
+        {
+            Log.i(TAG, "onActivityCreated");
+        }
+    };
 
     private final ActivityDestroyedObserver mActivityDestroyedObserver = new ActivityDestroyedObserver(this)
     {
