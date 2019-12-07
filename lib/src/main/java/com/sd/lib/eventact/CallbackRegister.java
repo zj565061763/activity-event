df19c1c9009596c15fcdc4f70072ee86080c4ebe
zjun <565061763@qq.com>
2019-12-07 20:16:36 +0800

    CallbackRegister 日志

--- lib/src/main/java/com/sd/lib/eventact/CallbackRegister.java
+++ lib/src/main/java/com/sd/lib/eventact/CallbackRegister.java
@@ -1,6 +1,7 @@
 package com.sd.lib.eventact;
 
 import android.app.Activity;
+import android.util.Log;
 
 import com.sd.lib.eventact.callback.ActivityEventCallback;
 
@@ -14,6 +15,11 @@ import java.util.WeakHashMap;
 class CallbackRegister
 {
     private Map<Activity, Map<Class<? extends ActivityEventCallback>, Collection<? extends ActivityEventCallback>>> mMapCallback;
+
+    private boolean isDebug()
+    {
+        return BuildConfig.DEBUG;
+    }
 
     public <T extends ActivityEventCallback> boolean register(Activity activity, Class<T> callbackClass, T callback)
     {
@@ -40,26 +46,40 @@ class CallbackRegister
             mapActivityCallback.put(callbackClass, callbacks);
         }
 
-        return callbacks.add(callback);
+        final boolean result = callbacks.add(callback);
+
+        if (isDebug())
+        {
+            Log.i(CallbackRegister.class.getName(),
+                    "+++++ register " + "\r\n" +
+                            "result:" + result + "\r\n" +
+                            "size:" + mMapCallback.size() + "," + mapActivityCallback.size() + "," + callbacks.size() + "\r\n" +
+                            "activity:" + activity + "\r\n" +
+                            "callbackClass:" + callbackClass + "\r\n" +
+                            "callback:" + callback
+            );
+        }
+
+        return result;
     }
 
-    public <T extends ActivityEventCallback> void unregister(Activity activity, Class<T> callbackClass, T callback)
+    public <T extends ActivityEventCallback> boolean unregister(Activity activity, Class<T> callbackClass, T callback)
     {
         if (activity == null || callbackClass == null || callback == null)
-            return;
+            return false;
 
         if (mMapCallback == null)
-            return;
+            return false;
 
         final Map<Class<? extends ActivityEventCallback>, Collection<? extends ActivityEventCallback>> mapActivityCallback = mMapCallback.get(activity);
         if (mapActivityCallback == null)
-            return;
+            return false;
 
         final Collection callbacks = mapActivityCallback.get(callbackClass);
         if (callbacks == null)
-            return;
+            return false;
 
-        callbacks.remove(callback);
+        final boolean result = callbacks.remove(callback);
 
         if (callbacks.isEmpty())
         {
@@ -71,6 +91,20 @@ class CallbackRegister
                     mMapCallback = null;
             }
         }
+
+        if (isDebug())
+        {
+            Log.i(CallbackRegister.class.getName(),
+                    "----- unregister " + "\r\n" +
+                            "result:" + result + "\r\n" +
+                            "size:" + (mMapCallback != null ? mMapCallback.size() : 0) + "," + mapActivityCallback.size() + "," + callbacks.size() + "\r\n" +
+                            "activity:" + activity + "\r\n" +
+                            "callbackClass:" + callbackClass + "\r\n" +
+                            "callback:" + callback
+            );
+        }
+
+        return result;
     }
 
     public <T extends ActivityEventCallback> Collection<T> get(Activity activity, Class<T> callbackClass)
