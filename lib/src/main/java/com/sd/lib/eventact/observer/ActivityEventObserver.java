ba2ebdb58eba3502151d9cf9fccb0557e49d6afc
zjun <565061763@qq.com>
2019-12-08 11:12:09 +0800

    ActivityEventObserver

--- lib/src/main/java/com/sd/lib/eventact/observer/ActivityEventObserver.java
+++ lib/src/main/java/com/sd/lib/eventact/observer/ActivityEventObserver.java
@@ -8,7 +8,7 @@ public interface ActivityEventObserver
      * 注册
      *
      * @param activity 要监听的对象
-     * @return
+     * @return true-观注册成功；false-注册失败或者已经注册到该Activity对象
      */
     boolean register(Activity activity);
 
