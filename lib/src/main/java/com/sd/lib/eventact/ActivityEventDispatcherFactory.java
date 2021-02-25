1aeb533cce1e0b5741907f14466e0a9987ecf8b4
zjun <565061763@qq.com>
2021-02-25 16:19:04 +0800

    annotation

--- lib/src/main/java/com/sd/lib/eventact/ActivityEventDispatcherFactory.java
+++ lib/src/main/java/com/sd/lib/eventact/ActivityEventDispatcherFactory.java
@@ -2,13 +2,15 @@ package com.sd.lib.eventact;
 
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
+
 public final class ActivityEventDispatcherFactory
 {
     private ActivityEventDispatcherFactory()
     {
     }
 
-    public static ActivityEventDispatcher create(Activity activity)
+    public static ActivityEventDispatcher create(@NonNull Activity activity)
     {
         return ActivityEventManager.getInstance().newActivityEventDispatcher(activity);
     }
